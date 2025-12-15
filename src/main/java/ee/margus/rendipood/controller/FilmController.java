abcaadd455b887dd28f603b28b99a35a9747e366
Margus <98420540+marguspollm@users.noreply.github.com>
2025-12-15 13:59:01 +0200

    Add FilmService class

--- src/main/java/ee/margus/rendipood/controller/FilmController.java
+++ src/main/java/ee/margus/rendipood/controller/FilmController.java
@@ -2,7 +2,7 @@ package ee.margus.rendipood.controller;
 
 import ee.margus.rendipood.entity.Film;
 import ee.margus.rendipood.entity.FilmType;
-import ee.margus.rendipood.repository.FilmRepository;
+import ee.margus.rendipood.service.FilmService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,47 +11,31 @@ import java.util.List;
 @RestController
 public class FilmController {
     @Autowired
-    private FilmRepository filmRepository;
+    private FilmService filmService;
 
     @GetMapping("films")
     public List<Film> filmsInStore(@RequestParam(required = false) Boolean inStock){
-        if(inStock == null) return filmRepository.findAllByOrderByIdAsc();
-        return filmRepository.findByInStockOrderByIdAsc(inStock);
+        return filmService.filmsInStore(inStock);
     }
 
     @PostMapping("films")
     public List<Film> addFilm(@RequestBody Film film){
-        if(film.getId() != null) throw new RuntimeException("Can't add a film with an existing id!");
-        film.setInStock(true);
-        filmRepository.save(film);
-        return filmRepository.findAllByOrderByIdAsc();
+        return filmService.addFilm(film);
     }
 
     @PostMapping("films-multi")
     public List<Film> addFilms(@RequestBody List<Film> films) {
-        for (Film f : films) {
-            if (f.getId() != null) {
-                throw new RuntimeException("Can't add a film without an id");
-            }
-            f.setInStock(true);
-            filmRepository.save(f);
-        }
-        return filmRepository.findAllByOrderByIdAsc();
+        return filmService.addFilms(films);
     }
 
     @DeleteMapping("films/{id}")
     public List<Film> deleteFilm(@PathVariable Long id){
-        filmRepository.deleteById(id);
-        return filmRepository.findAllByOrderByIdAsc();
+        return filmService.deleteFilm(id);
     }
 
     @PatchMapping("films-type")
     public List<Film> updateFilm(@RequestParam Long id, @RequestParam FilmType type){
-        Film film = filmRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Film with id"+ id +" not found"));
-        film.setType(type);
-        filmRepository.save(film);
-        return filmRepository.findAllByOrderByIdAsc();
+        return filmService.updateFilm(id, type);
     }
 
 }
